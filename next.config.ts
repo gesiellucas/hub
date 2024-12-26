d918bd3472e219cab89969002db48cb80766c634
Gesiel Lucas Ferreira <32823495+gesiellucas@users.noreply.github.com>
2024-12-26 22:09:41 +0000

    setup: codespace cloud settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'studious-space-journey-54g4g4vjj5r3pvw9-3000.app.github.dev']
+    }, // or other relevant flags
+  },
 };
 
 export default nextConfig;
